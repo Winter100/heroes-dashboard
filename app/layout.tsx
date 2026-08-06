b7686c104d72d6a468d356dbac18bb681d5a4ef2
winter100 <100taeng@gmail.com>
2026-08-06 13:13:20 +0900

    feat: tanstack-query추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Inter } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { TooltipProvider } from '@/components/ui/tooltip';
+import TanstackQueryProvider from '@/components/provider/tanstack-query-provider';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -35,7 +36,9 @@ export default function RootLayout({ children }: LayoutProps<'/'>) {
       )}
     >
       <body className='min-h-full flex flex-col dark'>
-        <TooltipProvider>{children}</TooltipProvider>
+        <TanstackQueryProvider>
+          <TooltipProvider>{children}</TooltipProvider>
+        </TanstackQueryProvider>
       </body>
     </html>
   );
